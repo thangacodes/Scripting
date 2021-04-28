f3f1e8fbf228ed41c49915a3055efa2292d0a851
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2021-04-28 21:36:13 +0530

    Create variable.sh
    Initial commit

---
+++ variable.sh
@@ -0,0 +1,14 @@
+## This is the file shows us, how to work on variable
+
+################## SCRIPT starts here###############
+
+# !/bin/bash
+echo $0
+echo $1
+echo $3
+echo $2
+echo $*  
+echo $#  
+
+## Note: $* mean points out all arguments in one line as a output
+         $# mean counts the arguments shows in output.
