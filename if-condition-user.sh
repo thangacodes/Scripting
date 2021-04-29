2919434fb0825bfc14c19b1aac9a0b7b3d769703
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2021-04-29 07:30:31 +0530

    Create if-condition-user.sh
    Initial commit

---
+++ if-condition-user.sh
@@ -0,0 +1,17 @@
+#### This script is adding /removing user accounts in the systems, by pressing any numbers
+
+##### Script starts here ##############
+
+#!/bin/bash
+echo "Enter 1 to add user"
+echo "Enter 2 to delete user"
+read -p "Enter the number:" NUMBER
+if [ $NUMBER -eq 1 ]; then
+    read -p "Enter the user name to add:" USER
+    useradd $USER
+elif [ $NUMBER -eq 2 ] ; then
+      read -p "Enter the user name to delete:" USER
+      userdel -r $USER
+else
+     echo "enter ONLY the number 1 or 2 "
+fi
