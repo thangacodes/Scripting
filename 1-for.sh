2dfbc480af176cf93cad3aa3543b67aaa1f82c69
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2021-04-30 22:46:21 +0530

    Update 1-for.sh
    Updated sleep command

--- 1-for.sh
+++ 1-for.sh
@@ -1,12 +1,17 @@
 ### Thi is the script to create bunch of users in same name
 
-Example, i wanted to created nisuser1....to nisuser20. Here nisuser is static name, only the numbers changing at the end of name. The numbers what we are calling it as "Dynamic numbers"
+Example, i wanted to created similar users like nisuser1..20. Here nisuser is static name, only the numbers changing at the end of name. 
+The numbers that what we are calling it as "Dynamic numbers".
 
 ######################## Script Starts here ######################
 
 #!/bin/bash
+
 for USER in {1..20}
+
 do
-      useradd nisuser $USER
-      echo "redhat123" | passwd --stdin nisuser$USER
+         echo "adding user $USER"
+         useradd niuser$USER
+         echo "redhat123" | passwd --stdin niuser$USER
+         sleep 2
 done
