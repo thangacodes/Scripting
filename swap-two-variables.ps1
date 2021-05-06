bcc928acc32397235f03e6229582b1e8e1b4e516
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2021-05-06 22:52:07 +0530

    Create swap-two-variables.ps1
    Initial commit

---
+++ swap-two-variables.ps1
@@ -0,0 +1,28 @@
+### How to swap two variables in Powershell 
+
+
+##### Script starts here ######
+
+
+Write-Host "How to swap two variables in Powershell" -ForegroundColor Red -BackgroundColor Black
+
+$a = Read-Host "Enter the value, you would like to pass" 
+$a
+$b = Read-Host "Enter the value, you would like to pass"
+$b
+Start-Sleep -Seconds 1
+
+# swapping two variables
+
+$a, $b = $b, $a
+
+Start-Sleep -Seconds 1
+
+Write-Host "The value of a is:" $a -ForegroundColor Red -BackgroundColor Black
+
+Start-Sleep -Seconds 1
+
+Write-Host "The value of b is:" $b -ForegroundColor Red -BackgroundColor Black
+
+
+### Note: This is PowerShell Script, which works on Windows Based OS
