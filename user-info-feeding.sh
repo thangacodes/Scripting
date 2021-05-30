c69c193b07dbc88cdc65ab006e18f664023c8066
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2021-05-30 12:12:15 +0530

    Create user-info-feeding.sh
    Initial commit

---
+++ user-info-feeding.sh
@@ -0,0 +1,7 @@
+### Input getting from the user and printing that values"
+
+#!/bin/bash
+echo -n "Enter your name: "; read name
+echo -n "Enter your mobile number: "; read number
+echo -n "Enter your native place: "; read place
+echo "Your name is $name and your native $place with the mobile number is $number"
