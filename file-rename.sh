bdaaca2e3afc6db0f3d52a0ffbe7c551d4a84bdb
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2021-07-22 21:58:08 +0530

    Update file-rename.sh

--- file-rename.sh
+++ file-rename.sh
@@ -20,14 +20,23 @@ fi
 #######  Another method to rename a file and show us, what has done during the execution 
 
 #!/bin/bash
-
 read -p "Enter the original filename to rename:" original
-
 read -p "Enter the renamed filename to rename:" rename
+sleep 1
+
 if [ -f $original ]; then
       $(mv -i $original $rename)
-      echo " The file is renamed as: $rename "
+      echo " The file is renamed as:" $rename 
       ls -l /home/centos/
+      sleep 1
+      echo "renaming file context showing"
+      sleep 2
+      cat $rename
 fi
 sleep 1
 exit
+
+### Note: read is a bash builtin command to read input from user. read command reads only a single line from bash shell. 
+Following is the syntax of read command,
+
+read <variable_name>
