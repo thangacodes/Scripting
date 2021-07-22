845d8015957639cd84c7c427b53749193df5b563
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2021-07-22 18:05:10 +0530

    Update prompt_variables.sh

--- prompt_variables.sh
+++ prompt_variables.sh
@@ -1,5 +1,5 @@
 #### Demo on read and print from the command line
 
 #!/bin/bash
-read -p "Enter your name :" NAME
-echo -e " Welcome to MyWorld MR.$NAME "
+read -p " Enter your name : " NAME
+echo -e " Welcome to MyWorld mr.$NAME "
