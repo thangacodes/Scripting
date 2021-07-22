fa5d177a7d36d0494a74ee77d3a3eb97bff7c832
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2021-07-22 23:00:14 +0530

    Update basics_of_function.sh
    second commit

--- basics_of_function.sh
+++ basics_of_function.sh
@@ -12,5 +12,14 @@ Linux
 [centos@ip-172-31-36-220 ss]$ d
 2021-07-22
 
+######### OTHER METHOD TO WRITE UP FUNCTION IN SHELL SCRIPT
+
+#!/bin/bash
+echo "This is going to be show us the use cases of the FUNCTION in Shell Scripting"
+sleep 1
+read -p "Enter your name please: " NAME
+v() { echo "Welcome to the Cloud World " ; echo "Todays date is $(date +%F)" ; sleep 1 ; echo " Number of users logged into the system is : $(who | wc -l)" ; }
+sleep 1
+v
 
 
