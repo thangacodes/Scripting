84c98b0681fa9d53d4a4a5986aed83e513d6f10a
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2022-03-14 13:44:45 +0530

    Create Jenkins.sh
    initial commit

---
+++ Jenkins.sh
@@ -0,0 +1,19 @@
+# How to install Jenkins on CentOS 7 using shell script
+
+
+#!/bin/bash
+sudo yum install epel-release -y
+sudo yum install java -y
+sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo --no-check-certificate
+sudo rpm --import http://pkg.jenkins.io/redhat-stable/jenkins.io.key
+sudo yum install jenkins -y
+sudo vi /etc/sysconfig/jenkins
+sleep 2
+echo "Going to change the custom port number in jenkins file"
+sudo sed -i 's/8080/8090/g' /etc/sysconfig/jenkins
+sudo cat /etc/sysconfig/jenkins | grep '80'
+sleep 2
+sudo systemctl status jenkins
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+
