208dfc48feb677adcecf493b133cb35e2bcd65d1
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2022-03-16 14:49:07 +0530

    Update and rename two-numbers.sh to adding.sh
    file name got changed

---
+++ adding.sh
@@ -0,0 +1,12 @@
+# How to add two numbers using shell script
+
+#!/bin/bash
+read -p "enter the first number: " ONE
+echo $ONE
+read -p "enter the second number: " SEC
+echo $SEC
+sleep 1
+echo -e "\e[1;32m ****** Going to add both the numbers $ONE, $SEC entered by an users *******"
+c=`expr $ONE + $SEC`
+echo -e "\e[1;32m ******addition of two number is $c *******"
+
