578f3fd3054e0501cf379ba7547ebebfcb41347a
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2022-04-19 15:22:16 +0530

    Update adding.sh
    date and timeframe updated on the script

--- adding.sh
+++ adding.sh
@@ -1,6 +1,7 @@
 # How to add two numbers using shell script
 
 #!/bin/bash
+echo "This script is executed at" $(date '+%Y-%m-%d %H:%M:%S')
 read -p "enter the first number: " ONE
 echo $ONE
 read -p "enter the second number: " SEC
