5615b6460eebbd86d58e2cf6f2415de0fa3990bc
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2022-04-20 08:50:15 +0530

    Update Jenkins.sh

--- Jenkins.sh
+++ Jenkins.sh
@@ -5,6 +5,7 @@
 echo -e "\e[1;32m ***** This script is executed at *****" $(date '+%Y-%m-%d %H:%M:%S')
 sudo yum install epel-release -y
 sudo yum install java -y
+sudo yum install -y wget
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo --no-check-certificate
 sudo rpm --import http://pkg.jenkins.io/redhat-stable/jenkins.io.key
 sudo yum install jenkins -y
