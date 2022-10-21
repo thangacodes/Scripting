6f653e42b415fad093133e71b761dbe795e2fb48
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2022-10-21 11:14:39 +0530

    Update ssl_csr_gent.sh

--- ssl_csr_gent.sh
+++ ssl_csr_gent.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# set -x
+# set -x     //Debug - It depends if you want to enable remove '#'
 
-echo -e "\e[1;32m @@@@@@@@ CSR GENERATION SCRIPT @@@@@@@@@@@@ \e[0m"
+echo -e "\e[1;32m @@@@@@@@ CSR GENERATION SCRIPT @@@@@@@@@@@@ "
 
 echo -e "\e[1;33m @@@@@@ This script is executed at @@@@@@: " $(date '+%Y-%m-%d %H:%M:%S')
 
@@ -11,8 +11,6 @@ echo -e "\e[1;34m @@@@@@ OpenSSL package checking @@@@@@ "
 sudo yum list installed | grep -i 'mod_ssl'
 
 echo -e "\e[1;35m  @@@@@@ BASIC CHECKOUTS ARE DONE  @@@@@@ "
-
-sleep 5
 
 echo -e "\e[1;36m @@@@@@@@     BEGIN WITH CSR GENERATION     @@@@@@@@@@@ "
 
@@ -24,22 +22,40 @@ if [ "$ENTER" = yes ] ;
 then
         echo -e "\e[1;32m @@@@@@  GOOD TO GO @@@@@@ "
 else
-        echo -e "\e[1:33m @@@@@@  SCRIPT EXITED.SINCE YOU'VE SELECTED AS NO @@@@@@ "
+        echo -e "\e[1:33m @@@@@@  SCRIPT EXITED.SINCE YOU'VE SELECTED NO or ANYTHING @@@@@@ "
 
         exit
 fi
 
 sleep 4
 
-openssl req -new -nodes -newkey rsa:2048 -subj "/CN=web01.dev.aws.example.com/C=IN/L=Bangalore/O=Example Softtech corp/OU=ITOPS/ST=Karnataka/emailAddress=TECH_OPS@example.com/subjectAltName=web01.dev.aws.example.com" -keyout web01.key -out web01.csr
+read -p "ENTER THE HOSTNAME(FQDN) THAT YOU WANT TO HAVE AN INTERNAL CSR TO BE GENERATED:" HOST1
 
-openssl req -new -nodes -newkey rsa:2048 -subj "/CN=web02.dev.aws.example.com/C=IN/L=Bangalore/O=Example Softtech corp/OU=ITOPS/ST=Karnataka/emailAddress=TECH_OPS@example.com/subjectAltName=web02.dev.aws.example.com" -keyout web02.key -out web02.csr
+openssl req -new -nodes -newkey rsa:2048 -subj "/CN=$HOST1/C=IN/L=Bangalore/O=Example Softtech corp/OU=ITOPS/ST=Karnataka/emailAddress=TECH_OPS@example.com/subjectAltName=$HOST1" -keyout $HOST1.key -out $HOST1.csr
 
-openssl req -new -nodes -newkey rsa:2048 -subj "/CN=web03.dev.aws.example.com/C=IN/L=Bangalore/O=Example Softtech corp/OU=ITOPS/ST=Karnataka/emailAddress=TECH_OPS@example.com/subjectAltName=web03.dev.aws.example.com" -keyout web03.key -out web03.csr
+read -p "ENTER THE HOSTNAME(FQDN) THAT YOU WANT TO HAVE AN INTERNAL CSR TO BE GENERATED:" HOST2
 
-openssl req -new -nodes -newkey rsa:2048 -subj "/CN=web04.dev.aws.example.com/C=IN/L=Bangalore/O=Example Softtech corp/OU=ITOPS/ST=Karnataka/emailAddress=TECH_OPS@example.com/subjectAltName=web04.dev.aws.example.com" -keyout web04.key -out web04.csr
 
-openssl req -new -nodes -newkey rsa:2048 -subj "/CN=web05.dev.aws.example.com/C=IN/L=Bangalore/O=Example Softtech corp/OU=ITOPS/ST=Karnataka/emailAddress=TECH_OPS@example.com/subjectAltName=web05.dev.aws.example.com" -keyout web05.key -out web05.csr
+openssl req -new -nodes -newkey rsa:2048 -subj "/CN=$HOST2/C=IN/L=Bangalore/O=Example Softtech corp/OU=ITOPS/ST=Karnataka/emailAddress=TECH_OPS@example.com/subjectAltName=$HOST2" -keyout $HOST2.key -out $HOST2.csr
+
+read -p "ENTER THE HOSTNAME(FQDN) THAT YOU WANT TO HAVE AN INTERNAL CSR TO BE GENERATED:" HOST3
+
+
+openssl req -new -nodes -newkey rsa:2048 -subj "/CN=$HOST3/C=IN/L=Bangalore/O=Example Softtech corp/OU=ITOPS/ST=Karnataka/emailAddress=TECH_OPS@example.com/subjectAltName=$HOST3" -keyout $HOST3.key -out $HOST3.csr
+
+
+read -p "ENTER THE HOSTNAME(FQDN) THAT YOU WANT TO HAVE AN INTERNAL CSR TO BE GENERATED:" HOST4
+
+
+openssl req -new -nodes -newkey rsa:2048 -subj "/CN=$HOST4/C=IN/L=Bangalore/O=Example Softtech corp/OU=ITOPS/ST=Karnataka/emailAddress=TECH_OPS@example.com/subjectAltName=$HOST4" -keyout $HOST4.key -out $HOST4.csr
+
+
+read -p "ENTER THE HOSTNAME(FQDN) THAT YOU WANT TO HAVE AN INTERNAL CSR TO BE GENERATED:" HOST5
+
+openssl req -new -nodes -newkey rsa:2048 -subj "/CN=$HOST5/C=IN/L=Bangalore/O=Example Softtech corp/OU=ITOPS/ST=Karnataka/emailAddress=TECH_OPS@example.com/subjectAltName=$HOST5" -keyout $HOST5.key -out $HOST5.csr
+
+
+
 
 ls -l | cut -d " " -f17 | grep csr > csr.txt
 
@@ -48,7 +64,7 @@ sleep 3
 for i in $(cat csr.txt);
 do
         echo -e "\e[1;34m @@@@@@@ Checking the generated csr files are VALID or NOT @@@@@@@ : $i"
-        sleep 3
+        # sleep 3
         openssl req -text -noout -verify -in $i | grep -i 'status'
 done
 
