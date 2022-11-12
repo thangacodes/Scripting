69c91e53cfe0961dc6f4959184ba26fd121d1291
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2022-11-12 17:05:33 +0530

    Update main.tf

--- Iac_TF/main.tf
+++ Iac_TF/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "tomcat_instance" {
     instance_type = var.instance_type
     key_name = var.key
     vpc_security_group_ids = [var.sgp]
-    user_data = file(apache.sh)
+    user_data = file("./apache.sh")
     tags = {
         Name = merge(var.taggy, { Name = "Apache-Tomcat-SVR" })
     }
