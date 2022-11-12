e3c52525cec5b7ab0285a7b79cab5a34bdd40b64
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2022-11-12 18:10:25 +0530

    Update main.tf

--- Iac_TF/main.tf
+++ Iac_TF/main.tf
@@ -1,8 +1,9 @@
 resource "aws_instance" "tomcat_instance" {
-    ami = var.ami_id
-    instance_type = var.instance_type
-    key_name = var.key
-    vpc_security_group_ids = ["var.sgp"]
-    user_data = file("./apache.sh")
-    tags = merge(var.taggy, { Name = "Apache-Tomcat-SVR" })
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = var.key
+  vpc_security_group_ids = var.sgp
+  subnet_id              = "subnet-e9190a81"
+  user_data              = file("apache.sh")
+  tags                   = merge(var.taggy, { Name = "Apache-Tomcat-SVR" })
 }
