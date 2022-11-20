219945e957977c6dadc1423d7c4a2d6e91875497
Murugan <thangadurai.murugan@tesco.com>
2022-11-20 20:24:52 +0530

    tagging is changed

--- Iac_TF/mysql_workouts/ec2.tf
+++ Iac_TF/mysql_workouts/ec2.tf
@@ -5,5 +5,5 @@ resource "aws_instance" "ansible_instance" {
   key_name               = var.key
   vpc_security_group_ids = var.sgp
   subnet_id              = "subnet-e9190a81"
-  tags                   = merge(var.taggy, { Name = "Ansible-client-machine-${count.index + 1}" })
+  tags                   = merge(var.taggy, { Name = "Ansible-mysql-machine-${count.index + 1}" })
 }
