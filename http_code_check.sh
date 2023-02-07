a066a59e0939e77099ef96f586d6ace9f2a0d844
Thangadurai M <38905243+send2durai@users.noreply.github.com>
2023-02-07 22:11:29 +0530

    Update http_code_check.sh

--- http_code_check.sh
+++ http_code_check.sh
@@ -1,15 +1,14 @@
 #!/bin/sh
 
 #set -x  # Enabling Debugging
-read -p "Enter the URL that you wanted to test:" URL
+#read -p "Enter the URL that you wanted to test:" URL
+URL='https://www.google.com/'
 echo $URL
 echo $URL
 echo "Script is to check client response code on $URL"
-
+sleep 5
 CURL=`curl --insecure -I $URL -o /dev/null -w '%{http_code}\n' -s`
-
 echo "http client response code on node: $(hostname)": $CURL
-
 if [ "$CURL" = 200 ]
 then
      echo "health check looks good and hc status code is 200"
