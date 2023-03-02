2b37201dc4a8828186178c37781de926aa03f205
Murugan <thangadurai.murugan@tesco.com>
2023-03-03 02:32:03 +0530

    color coding added

--- awscli_scripts/security_group.sh
+++ awscli_scripts/security_group.sh
@@ -1,18 +1,20 @@
 #!/bin/sh
-echo -e "\e[1;32m ***** This script is executed at *****" $(date '+%Y-%m-%d %H:%M:%S')
+# set -x //Debug enabling when do you require it...
+echo -e "\e[1;34m ***** This script is executed at *****" $(date '+%Y-%m-%d %H:%M:%S')
 echo "Adding security group inbound rules on the existing security group"
+
 read -p "Enter the Security_group_id please:" SECGP
-echo $SECGP
+echo -e "\e[1;31m ***** You've entered the security_group id is as: *****" $SECGP
 read -p "Enter the IP CIDR please:" IPCIDR
-echo $IPCIDR
+echo -e "\e[1;32m ***** You've entered the IP_CIDR value is as: *****" $IPCIDR
 read -p "Enter the reasonable description:" DESC
-echo $DESC
-read -p "Enter the fromport number:" FROMPORT
-echo $FROMPORT
-read -p "Enter the toport number:" TOPORT
-echo $TOPORT
+echo -e "\e[1;33m ***** You've entered the Description is as: *****" $DESC
+read -p "Enter the fromport:" FROMPORT
+echo -e "\e[1;34m ***** You've entered the From_port value as: *****" $FROMPORT
+read -p "Enter the toport:" TOPORT
+echo -e "\e[1;35m ***** You've entered the To_port value as: *****" $TOPORT
 read -p "Enter the region where you're security_group is existing:" REGION
-echo $REGION
+echo -e "\e[1;36m ***** You've entered the Region in AWS is as: *****" $REGION
 echo "******************** LETS PROCEED WITH AUTOMATION OF AWSCLI WITH BASH SCRIPT ************************"
 aws ec2 authorize-security-group-ingress --group-id $SECGP --ip-permissions IpProtocol=tcp,FromPort=$FROMPORT,ToPort=$TOPORT,IpRanges="[{CidrIp=$IPCIDR,Description='$DESC'}]"
 echo "Describing a security group:" $SECGP
@@ -25,11 +27,9 @@ echo -e "\e[1;35m ***** @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ **
 echo -e "\e[1;36m ***** @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ ***** "
 echo -e "\e[1;37m ***** @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ ***** "
 read -p "Are we good to proceed in revoking the SG rules:" ENTER
-echo "You've entered value as:" $ENTER
-echo "You've entered value as:" $ENTER
-echo "Before proceeding with revoking access, there will be a small break ....."
+echo -e "\e[1;37m ***** You've feed the value as: *****" $ENTER
+echo -e "\e[1;34m ***** Before proceeding with revoking access, there will be a small break: *****"
 sleep 5
-echo "revoking security_group_rules in progress............"
+echo -e "\e[1;36m ***** Revoking security_group_rules in progress: *****"
 aws ec2 revoke-security-group-ingress --group-id $SECGP --ip-permissions IpProtocol=tcp,FromPort=$FROMPORT,ToPort=$TOPORT,IpRanges="[{CidrIp=$IPCIDR,Description='$DESC'}]"
 exit
-
