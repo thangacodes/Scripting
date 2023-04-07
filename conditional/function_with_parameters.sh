1f66241970131d78254dbcc4dc64274691f9caba
Murugan <thangadurai.murugan@tesco.com>
2023-04-07 20:00:36 +0530

    params in shell_script

---
+++ conditional/function_with_parameters.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "The script is execute at:" $(date '+%Y.%m.%d %H:%M:%S')
+function greet(){
+    echo "Hello, I am $1 and I am $2 years old and working in IT Sector"
+}
+greet "Thangadurai" "34"
