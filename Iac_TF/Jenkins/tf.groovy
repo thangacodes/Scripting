94a302c24ba04bc8fc841341c4d4b9ae437e6268
Murugan <thangadurai.murugan@tesco.com>
2023-07-13 19:15:15 +0530

    Jenkins with groovy creation

---
+++ Iac_TF/Jenkins/tf.groovy
@@ -0,0 +1,35 @@
+def tfinit() {
+    echo 'terraform init in progress...'
+    sh '''
+         cd ../ami_data/
+         pwd
+         terraform init
+       '''      
+} 
+def tffmt() {
+    echo 'terraform format in progress...'
+    sh '''
+         cd ../ami_data/
+         pwd
+         terraform fmt
+      '''
+    
+} 
+def tfvalidate() {
+    echo 'terraform validate in progress...'
+    sh '''
+          cd ../ami_data/
+          pwd
+          terraform validate
+      '''
+       
+} 
+def tfplan() {
+    echo 'terraform plan in progress...'
+    sh '''
+         cd ../ami_data/
+         pwd
+         terraform plan
+      '''
+} 
+return this
