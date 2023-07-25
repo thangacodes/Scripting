a0bca92149fe31550c1b79d9e5fc844e54a9fa0b
Murugan <thangadurai.murugan@tesco.com>
2023-07-25 09:18:26 +0530

    user input read

---
+++ user_input_login.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+read -p "Enter first name: " fname
+echo "First Name is: " ${fname}
+read -t 5 -p "Enter Last name: " lname
+echo "Last name is: " ${lname}
+read -s -p "Password: " password
+echo "Your password is: " ${password}
+
+if [[ ${password} == "login123" ]]
+then
+echo "You've entered the correct password"
+elif [[ ${password} == "login456" ]]
+then
+echo "You've entered wrong password"
+else
+echo "Nothing is right..."
+fi
