0ea3a73d22864f1c5fdd32f82c187b22939161e7
Murugan <thangadurai.murugan@tesco.com>
2023-08-14 21:30:12 +0530

    color coding changed

--- inventory.sh
+++ inventory.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
-echo -e "\033[91m Getting system information in an inventory file"
-Date=$(date '+%Y-%m-%d %H:%M:%S')
-echo "The date is:" ${Date}
-Uptime=$(uptime -p)
-echo "The uptime of the server is:" ${Uptime}
+
+echo -e "\033[94m Getting system information"
+DATE=$(date '+%Y-%m-%d %H:%M:%S')
+echo "The date is:" ${DATE}
+UPTIME=$(uptime -p)
+echo "The uptime of the server is:" ${UPTIME}
+HOSTNAME=$(hostname)
+echo "The server hostname is:" ${HOSTNAME}
 IP_ADDRESS=$(hostname -i)
 echo "Ip Address of the server is:" ${IP_ADDRESS}
 OS_NAME=$(uname)
-echo "Os version of the server is:" ${OS_NAME}
+echo "OS version of the server is:" ${OS_NAME}
 
-echo "S.No,Date,Uptime,IP_ADDRESS,OS_NAME" > inventory.csv
-echo "1.$Date,$Uptime,$IP_ADDRESS,$OS_NAME" >> inventory.csv
+echo "S.No,DATE,UPTIME,HOSTNAME,IP_ADDRESS,OS_NAME" > inventory.csv
+echo "1.$DATE,$UPTIME,$HOSTNAME,$IP_ADDRESS,$OS_NAME" >> inventory.csv
 
