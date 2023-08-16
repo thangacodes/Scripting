fd23043d0f3301fca5ed3adf4bb4d3169ec61d38
Murugan <thangadurai.murugan@tesco.com>
2023-08-16 23:51:38 +0530

    print value changed in echo

--- my_array.sh
+++ my_array.sh
@@ -2,8 +2,8 @@
 
 myarray=( 1 2 Hello "Hey man")
 
-echo "${myarray[0]}"
-echo "${myarray[1]}"
-echo "${myarray[2]}"
-echo "${myarray[3]}"
+echo "Value in 1st index is:" ${myarray[0]}
+echo "Value in 2nd index is:" ${myarray[1]}
+echo "Value in 3rd index is:" ${myarray[2]}
+echo "Value in 4th index is:" ${myarray[3]}
 
