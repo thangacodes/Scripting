c8610b0540a9f52f530eb74eff11f1c24e95fb75
send2durai <send2durai@hotmail.com>
2023-12-15 10:28:24 +0530

    starting service step added

--- jenkins_home_dir_change.sh
+++ jenkins_home_dir_change.sh
@@ -5,3 +5,5 @@ sudo chown jenkins:jenkins /home/jenkins_home
 sudo cp -rpv /var/lib/jenkins /home/jenkins_home
 sudo usermod -d /home/jenkins_home
 sudo vi /etc/default jenkins
+JENKINS_HOME=/home/jenkins_home
+sudo systemctl start jenkins.service
