3566496a943c08a8c3f95b40cf9a7190716ab5fe
send2durai <send2durai@hotmail.com>
2024-02-20 14:02:53 +0530

    newly argument added

--- besteverday.sh
+++ besteverday.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 name=$1
+complementry=$2
 read -p "Please enter your name :" name
+read -p "Please precious your complementry :" complementry
 echo "Good Morning $name!!"
 sleep 1
 echo "You're looking good today $name!!"
 sleep 1
-echo "You have the best beard I've ever seen $name!!"
+echo "You have the best $complementry I've ever seen $name!!"
