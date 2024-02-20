ff360a481fb3406d765c3d4dd45ca579a59d5302
send2durai <send2durai@hotmail.com>
2024-02-20 11:38:34 +0530

    initial commit on arg

---
+++ besteverday.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+name=$1
+read -p "Please enter your name :" name
+echo "Good Morning $name!!"
+sleep 1
+echo "You're looking good today $name!!"
+sleep 1
+echo "You have the best beard I've ever seen $name!!"
