93fb678f730acfd46bc586c68be69054519fa01e
send2durai <send2durai@hotmail.com>
2024-02-21 07:52:58 +0530

    script modified

--- besteverday.sh
+++ besteverday.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-
 name=$1
 complementary=$2
 read -p "Please enter your name :" name
-read -p "Please precious your complementry :" complementry
+read -p "Please precious your complementary :" complementary
 echo "Good Morning $name!!"
 sleep 1
 echo "You're looking good today $name!!"
 sleep 1
-echo "You have the best $complementry I've ever seen $name!!"
+echo "You have the best $complementary I've ever seen $name!!"
