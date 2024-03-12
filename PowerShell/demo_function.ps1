619a2152dc14ea9267923a80be1d7f2a963e0e31
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2024-03-12 14:16:02 +0530

    Update demo_function.ps1

--- PowerShell/demo_function.ps1
+++ PowerShell/demo_function.ps1
@@ -1,10 +1,10 @@
 function Get-greetings {
-    $greet = "Thangadurai"
-    Write-Host $greet -ForegroundColor Green -BackgroundColor Black
+    $greet = Read-Host "Please enter your name"
+    Write-Host "Your name is $greet" -f Green -BackgroundColor Black
     $family_name = Read-Host "Please enter your family name please"
+    Write-Host "Your family name is:" $family_name -f Green -BackgroundColor Black
     $age = Read-Host "What is your age, please tell us"
     Write-Host "Your age is:" $age -ForegroundColor Green -BackgroundColor Black
-    Write-Host "Your family name is:" $family_name -f Green -BackgroundColor Black
     Write-Host "Hello, $greet and your age is $age and family_name is: $family_name" -ForegroundColor Green -BackgroundColor Black
 }
 Get-greetings
