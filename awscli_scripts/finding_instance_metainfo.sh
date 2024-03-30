17c058d0d476da5d4d1b4ee0cab5c1ee508db4e4
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2024-03-30 21:46:59 +0530

    Update finding_instance_metainfo.sh

--- awscli_scripts/finding_instance_metainfo.sh
+++ awscli_scripts/finding_instance_metainfo.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-INSID=$(aws ec2 describe-instances | grep "InstanceId")
+FIND=$(aws ec2 describe-instances | grep "InstanceId")
 
-FIND=$(echo $INSID | cut -d'"' -f4)
+INSID=$(echo $FIND | cut -d'"' -f4)
 
 PRIVATE_IP=$(aws ec2 describe-instances --instance-ids "$FIND" --query 'Reservations[*].Instances[*].PrivateIpAddress')
 PUBLIC_IP=$(aws ec2 describe-instances --instance-ids "$FIND" --query 'Reservations[*].Instances[*].PublicIpAddress')
 SECURITY_GROUP=$(aws ec2 describe-instances --instance-ids "$FIND" --query 'Reservations[*].Instances[*].SecurityGroups[*].GroupName')
 
 ## Outputs
-echo "Instance Id is: $FIND"
+echo "Instance Id is: $INSID"
 echo "Instance Public IP is: $PRIVATE_IP"
 echo "Instance Private IP is: $PUBLIC_IP"
 echo "Instance Security Group is: $SECURITY_GROUP"
