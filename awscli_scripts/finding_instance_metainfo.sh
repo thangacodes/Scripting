659bc47991019e724f8301bbf1a15554c52f8533
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2024-03-30 21:58:31 +0530

    Update finding_instance_metainfo.sh

--- awscli_scripts/finding_instance_metainfo.sh
+++ awscli_scripts/finding_instance_metainfo.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-FIND=$(aws ec2 describe-instances | grep "InstanceId")
+# Get Instance ID
+INSID=$(aws ec2 describe-instances --query 'Reservations[*].Instances[*].InstanceId' --output text)
 
-INSID=$(echo $FIND | cut -d'"' -f4)
+# Loop through each Instance ID
+for ID in $INSID; do
+    # Get Private IP
+    PRIVATE_IP=$(aws ec2 describe-instances --instance-ids "$ID" --query 'Reservations[*].Instances[*].PrivateIpAddress' --output text)
+    # Get Public IP
+    PUBLIC_IP=$(aws ec2 describe-instances --instance-ids "$ID" --query 'Reservations[*].Instances[*].PublicIpAddress' --output text)
+    # Get Security Groups
+    SECURITY_GROUP=$(aws ec2 describe-instances --instance-ids "$ID" --query 'Reservations[*].Instances[*].SecurityGroups[*].GroupName' --output text)
 
-PRIVATE_IP=$(aws ec2 describe-instances --instance-ids "$FIND" --query 'Reservations[*].Instances[*].PrivateIpAddress')
-PUBLIC_IP=$(aws ec2 describe-instances --instance-ids "$FIND" --query 'Reservations[*].Instances[*].PublicIpAddress')
-SECURITY_GROUP=$(aws ec2 describe-instances --instance-ids "$FIND" --query 'Reservations[*].Instances[*].SecurityGroups[*].GroupName')
-
-## Outputs
-echo "Instance Id is: $INSID"
-echo "Instance Public IP is: $PRIVATE_IP"
-echo "Instance Private IP is: $PUBLIC_IP"
-echo "Instance Security Group is: $SECURITY_GROUP"
+    # Output that we required is like this..
+    echo "Instance Id is: $ID"
+    echo "Instance Public IP is: $PUBLIC_IP"
+    echo "Instance Private IP is: $PRIVATE_IP"
+    echo "Instance Security Group is: $SECURITY_GROUP"
+done
