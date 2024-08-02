0c33e709447de4c187d6f4bf3da6ffbd53e2887b
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2024-08-02 22:34:09 +0530

    Update cert_create.sh

--- openssl/cert_create.sh
+++ openssl/cert_create.sh
@@ -18,11 +18,17 @@ echo "Generating certificate..."
 # Generate a private key
 openssl genrsa -out "$DOMAIN.key" "$BYTE"
 
+# Generating a public key from existing private key
+openssl rsa -in "${DOMAIN}.key" -pubout -out "${DOMAIN}_pub.key"
+
 # Create a CSR (Certificate Signing Request)
-openssl req -new -key "$DOMAIN.key" -out "$DOMAIN.csr"
+openssl req -new -key "$DOMAIN.key" -out "$DOMAIN.csr" -subj "/C=IN/ST=KA/L=Bangalore/O=Cloudbird/OU=Cloudbird/CN=*.${DOMAIN}.fun/emailAddress=admin@${DOMAIN}.fun"
 
 # Verify the CSR
-openssl req -text -in "$DOMAIN.csr" -noout -verify
+openssl req -text -in "$DOMAIN.csr" -noout -verify | grep -i 'Certificate request self-signature ok'
+
+# Pause for 3 seconds
+sleep 3
 
 # Create a self-signed certificate from the CSR
 openssl x509 -in "$DOMAIN.csr" -out "$DOMAIN.crt" -req -signkey "$DOMAIN.key" -days "$DAYS"
