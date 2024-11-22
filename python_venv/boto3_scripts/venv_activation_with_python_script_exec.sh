30cfb2f334dc62082d807111bfb33cc6e06cbd95
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2024-11-22 16:56:30 +0530

    updation

--- python_venv/boto3_scripts/venv_activation_with_python_script_exec.sh
+++ python_venv/boto3_scripts/venv_activation_with_python_script_exec.sh
@@ -1,30 +1,30 @@
 #!/bin/bash
 
-echo "script to execute shell and python together after venv created..."
-
-read -r -p "Enter the folder name you want to use for the venv: " FOLDER
-
-echo "Your input on creating a folder name is: " $FOLDER
-
+echo 
+echo "Bash script to invoke a Python script after creating a virtual environment..."
+echo 
+read -r -p "Enter the name of the folder you want to use for the virtual environment: " FOLDER
+echo "Your input for the folder name is: " $FOLDER
+echo 
 python3 -m venv $FOLDER
-
 if [ -d "$FOLDER" ]; then
-echo "Venv folder $FOLDER is exist..."
+echo "virtual environment folder $FOLDER is exist..."
 else
-echo "Venv folder $FOLDER does not exist.."
+echo "virtual environment folder $FOLDER does not exist.."
 exit 1
 fi
 source "$FOLDER/bin/activate"
 echo "Installing required pip packages like boto3, pytz..."
+echo "Upgrading the pip package: "
 pip install --upgrade pip
+echo "Installing the boto3 pip package: "
 pip3 install boto3
+echo "Installing the pytz pip package: "
 pip3 install pytz
-echo "execute python script now..."
+echo "Invoking the Python script simple.py now: "
 python3 simple.py
-sleep 5
-echo "deactivating venv, post script execution..."
+echo "Deactivating the virtual environment after script execution: "
 deactivate
-sleep 5
-echo "remove the folder that we created for venv: " $FOLDER
+echo "Remove the folder we created for the virtual environment: " $FOLDER
 rm -rf $FOLDER
 exit 0
