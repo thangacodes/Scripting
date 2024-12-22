cb445f11d70dda9e51fe64def0810a8078f27696
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2024-12-22 10:52:48 +0530

    enhancement

--- shellscripts/devops_exp.sh
+++ shellscripts/devops_exp.sh
@@ -2,6 +2,8 @@
 # Display the current date and time when the script is executed
 echo "The script was executed at: $(date '+%d-%m-%Y %H:%M:%S')"
 # Ask for inputs
+echo "What is your good name?"
+read good_name
 echo "What is your experience in IT?"
 read experience
 echo "What is your Primary Skills?"
@@ -9,17 +11,18 @@ read primary_skills
 echo "What is your Secondary Skills?"
 read secondary_skills
 echo "Which cloud do you prefer to use and work with day in and day out?"
-read Cloud
+read cloud
 
 # Now we can assign the inputs to arguments (simulating positional parameters)
-set -- "$experience" "$primary_skills" "$secondary_skills" "$Cloud"
+set -- "$good_name" "$experience" "$primary_skills" "$secondary_skills" "$cloud"
 
 # Output the results as positional arguments
 echo "Your good name is: $1"
-echo "Your Skill sets are: $2"
-echo "Your Primary Cloud is: $3"
-echo "Your Secondary skill sets are: $4"
+echo "Your IT experience is: $2"
+echo "Your Primary skills sets are: $3"
+echo "Your Secondary skills are: $4"
+echo "Your interested in cloud: $5"
 
 # Combine all names and display them as a single string
-full_knowledge="You are having $1 years of experience and expertise in: $2, $3, $4"
+full_knowledge="My name is $1, and I have around $2 years of experience and expertise in: $3, $4, $5"
 echo $full_knowledge
