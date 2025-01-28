319c699b5d14d6dd4f81d1a5a0125a4446efa37f
Thangadurai <38905243+thangacodes@users.noreply.github.com>
2025-01-28 20:39:19 +0530

    updated script

--- python_venv/bash/create_delete_python_venv.sh
+++ python_venv/bash/create_delete_python_venv.sh
@@ -1,14 +1,36 @@
 #!/bin/bash
 show_date() {
-echo "Script execution time is: $(date '+%d-%m-%Y %H:%M:%S')"
-echo 
-echo "Script is to create a virtual environment using Python3"
-echo
+    echo "Script execution time is: $(date '+%d-%m-%Y %H:%M:%S')"
+    echo "Script is to create a virtual environment using Python3"
 }
 executor_detail() {
-USER=$(whoami)
-echo "Executor name is:" $USER
-echo
+    USER=$(whoami)
+    echo "Who is the executor of this job:" $USER
+}
+check_python_is_exist() {
+    echo "Checking if 'Python3' is installed or not:"
+    python3 --version &>/dev/null
+    if [[ $? -ne 0 ]]; then
+       echo "Python 3 is not installed. Installing Python3..."
+       if command -v apt-get &>/dev/null; then
+            sudo apt-get update && sudo apt-get install -y python3
+            echo "Python 3 installation complete!"
+        elif command -v yum &>/dev/null; then
+            sudo yum install -y python3
+            echo "Python 3 installation complete!"
+        elif command -v dnf &>/dev/null; then
+            sudo dnf install -y python3
+            echo "Python 3 installation complete!"
+        elif command -v brew &>/dev/null; then
+            brew install python3
+            echo "Python 3 installation complete!"
+        else
+            echo "Package manager not supported. Please install Python 3 manually."
+            exit 1
+        fi
+    else
+       echo "Python 3 is already installed."
+    fi
 }
 pyenv_creation() {
     PYENV_PATH="/Users/td/Desktop/hcv/bash_scripts"
@@ -49,7 +71,6 @@ pyenv_creation() {
     echo
     echo "Checking 'pip version'"
     pip --version
-    echo
 }
 deactivate_and_remove_venv() {
     read -p "Would you like to deactivate the Python3 venv? (yes/no):" USER_INPUT
@@ -66,7 +87,7 @@ deactivate_and_remove_venv() {
     echo 
     ls -lrt $PYENV_PATH
     echo 
-    echo "looks good.."
+    echo "Looks good, as all the files exist in the directory:" $(pwd)
     echo 
     elif [[ $USER_INPUT == "no" ]]; then
     echo "Please abort, since user input is 'no'.."
@@ -76,6 +97,7 @@ deactivate_and_remove_venv() {
 }
 show_date
 executor_detail
+check_python_is_exist
 pyenv_creation
 deactivate_and_remove_venv
 exit 0
